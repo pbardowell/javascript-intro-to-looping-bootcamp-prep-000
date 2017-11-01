9ddf3d9be9f6f66e4f5b714ad2d50a17afbe744f
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-11-01 23:12:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,3 +14,13 @@ function whileLoop (number) {
   }
   return "done"
 }
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do {
+  array.pop()
+}
+while (maybeTrue() && array.length > 0 )
+return array
+}
